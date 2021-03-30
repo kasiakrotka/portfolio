171e0e42c83665a2fbf02930a8f5a41c67af54ca
Katarzyna Krawczyk <frygtt@gmail.com>
2021-03-30 15:58:04 +0200

    add images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,8 +13,8 @@ export class AppComponent {
 
   constructor(){
     this.projectsArray = new Array(); 
-    this.projectsArray.push(new Project("Quiz","Simple quiz application with simple design and graph of given answers", "#", ['HTML', 'CSS', 'JavaScript'], "../assets/quiz.png" ));
-    this.projectsArray.push(new Project("Mood Tracker","Application let user track his mood through the month. The purpose of app is therapeutic. User have a chance to fill fomrs which help him describe his day. Data collected in the app can be displayed as a graph.", "#", ['HTML', 'CSS', 'JavaScript', 'Angular'], "../assets/moodtracker.png" ));
+    this.projectsArray.push(new Project("Quiz","Simple quiz application with simple design and graph of given answers", "#", ['HTML', 'CSS', 'JavaScript'], "assets/quiz.png" ));
+    this.projectsArray.push(new Project("Mood Tracker","Application let user track his mood through the month. The purpose of app is therapeutic. User have a chance to fill fomrs which help him describe his day. Data collected in the app can be displayed as a graph.", "#", ['HTML', 'CSS', 'JavaScript', 'Angular'], "assets/moodtracker.png" ));
     this.projectsNum = this.projectsArray.length;
   }
 
